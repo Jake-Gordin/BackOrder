41358a914ea83817c0a898a27790edd5d21cc92c
George Gordin <gcgordin@gmail.com>
2026-06-19 17:44:51 -0600

    changing IDs to random ints for delete route

--- backshelf/app.js
+++ backshelf/app.js
@@ -187,6 +187,7 @@ app.put('/items', (req, res) => {
 app.delete('/items', (req, res) => {
     const reqData = req.body;
     targetID = reqData.ID;
+    console.log(reqData)
     console.log("received request to delete item with ID: " + targetID)
     const sqlParams = [targetID];
     const queryText = `delete from items where ID = ?;`
@@ -197,7 +198,6 @@ app.delete('/items', (req, res) => {
         return;
       }
       else {
-        console.log(result)
         res.send("ITEM_DELETE_OK");
       }
     })
